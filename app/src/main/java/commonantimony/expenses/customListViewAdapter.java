b1eaa95cf7d63a13fbbd22e96480cb236ce20661
Sushant Bansal <1star.sushant@gmail.com>
2016-12-30 00:13:09 +0530

    Attempting List View

---
+++ app/src/main/java/commonantimony/expenses/customListViewAdapter.java
@@ -0,0 +1,12 @@
+package commonantimony.expenses;
+
+import android.app.ListActivity;
+
+/**
+ * Created by Sushant on 12/30/2016.
+ * Custom List View Adapter
+ */
+
+class customListViewAdapter extends ListActivity{
+
+}
