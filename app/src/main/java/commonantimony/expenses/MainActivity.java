286973b025246bf7194c5aace6bcd8a154d9e668
Sushant Bansal <1star.sushant@gmail.com>
2017-01-03 23:31:22 +0530

    ListView Functional with CustomView and ArrayAdapter

--- app/src/main/java/commonantimony/expenses/MainActivity.java
+++ app/src/main/java/commonantimony/expenses/MainActivity.java
@@ -1,16 +1,42 @@
 package commonantimony.expenses;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+class items {
+    String name;
+    float val;
+
+    items() {
+        this.name = "Item";
+        this.val = 00;
+    }
+}
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<items> listItemsArray = new ArrayList<>();
+    int list_count = 0;
+    ArrayAdapter lI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lI = new customListViewAdapter(this, listItemsArray);
+        ListView lV = (ListView) findViewById(R.id.listView);
+        lV.setDescendantFocusability(ViewGroup.FOCUS_AFTER_DESCENDANTS);
+        lV.setAdapter(lI);
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -18,5 +44,11 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.addbutton, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void addItem(MenuItem M) {
+        list_count++;
+        lI.add(new items());
+        lI.notifyDataSetChanged();
     }
 }
--- app/src/main/java/commonantimony/expenses/customListViewAdapter.java
+++ app/src/main/java/commonantimony/expenses/customListViewAdapter.java
@@ -1,12 +1,130 @@
 package commonantimony.expenses;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Sushant on 12/30/2016.
  * Custom List View Adapter
  */
 
-class customListViewAdapter extends ListActivity{
+class customListViewAdapter extends ArrayAdapter<items> {
 
+    ArrayList<items> listX = new ArrayList<>();
+    TextView totalValue;
+    float total;
+    Context ctxt;
+
+    public customListViewAdapter(Context context, ArrayList L) {
+        super(context, R.layout.custom_listview, L);
+        this.listX = L;
+        ctxt = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater lI = LayoutInflater.from(getContext());
+        View customView = lI.inflate(R.layout.custom_listview, parent, false);
+        totalValue = (TextView) ((Activity) ctxt).findViewById(R.id.amountTextView);
+        TextView sNo = (TextView) customView.findViewById(R.id.sNo);
+        final EditText name = (EditText) customView.findViewById(R.id.itemNameList);
+        name.setText(listX.get(position).name);
+        name.setBackgroundColor(Color.TRANSPARENT);
+        name.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try {
+                    listX.get(position).name = name.getText().toString();
+                } catch (Exception E) {
+
+                }
+
+            }
+        });
+
+        final EditText val = (EditText) customView.findViewById(R.id.itemValList);
+        try {
+            val.setText(String.valueOf(listX.get(position).val));
+        } catch (Exception E) {
+
+        }
+        val.setBackgroundColor(Color.TRANSPARENT);
+        val.setInputType(InputType.TYPE_CLASS_NUMBER);
+        val.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try {
+                    listX.get(position).val = Float.valueOf(val.getText().toString());
+                    total = addValues();
+                    totalValue.setText(String.valueOf(total));
+                    //Toast.makeText(ctxt, "Total:"+total, Toast.LENGTH_SHORT).show()                    ;
+                } catch (Exception E) {
+
+                }
+            }
+        });
+
+        ImageView close = (ImageView) customView.findViewById(R.id.removeItem);
+
+        sNo.setText((position + 1) + ".");
+        close.setImageResource(R.drawable.ic_delete_black_48dp);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listX.remove(position);
+                notifyDataSetChanged();
+                total = addValues();
+                totalValue.setText(String.valueOf(total));
+
+            }
+        });
+        return customView;
+    }
+
+    float addValues() {
+        float sum = 0;
+        int i = 0;
+        for (i = 0; i < listX.size(); i++) {
+            sum = sum + listX.get(i).val;
+        }
+//        Log.v("TAG","SUM = "+sum);
+        return sum;
+    }
 }
